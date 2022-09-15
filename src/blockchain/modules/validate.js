8a2464ab489d5d8766e6f3221d93f50cd07daa07
gerardo jerez <gerardo.jerez@hotmail.com>
2022-09-14 17:43:18 -0700

    validate

---
+++ src/blockchain/modules/validate.js
@@ -0,0 +1,16 @@
+import Block from "../block.js";
+
+export default(blockchain) => {
+    const [ genesisBlock, ...block ] = blockchain;
+    if(JSON.stringify(genesisBlock) !== JSON.stringify(Block.genesis)) throw Error('Bloque genesis invalido');
+
+    for(let i = 0; i < blocks.length; i++){
+
+        const { previousHash, timeStamp, hash, data } = blocks[i];
+
+        const previousBlock = blockchain[i];
+
+        if(previousHash !== previousBlock.hash) throw Error('Hash previo invalido o corrupto');
+        if(hash !== Block.hash(timeStamp, previousHash, data)) throw Error('Hash invalido');
+    }
+}
