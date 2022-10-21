06d15f92bd832aef94b22b829f5f3033100f1323
gerardo jerez <gerardo.jerez@hotmail.com>
2022-10-21 15:21:22 -0700

    hey

--- src/services/index.js
+++ src/services/index.js
@@ -2,12 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 
 import BlockChain from "../blockchain/blockchain.js";
-import { restart } from "nodemon";
+import P2PService from "./p2p.js";
 
 const { HTTP_PORT = 3000} = process.env;
 
 const app = express();
 const blockchain = new BlockChain();
+const p2pservice = new P2PService(blockchain);
 
 app.use(bodyParser.json());
 
@@ -19,7 +20,7 @@ app.post('/mine', (req, res) => {
     const { body: { data } } = req;
     const block = blockchain.addBlock(data);
 
-    restart.json({
+    res.json({
         blocks: blockchain.blocks.length,
         block,
     });
@@ -27,4 +28,5 @@ app.post('/mine', (req, res) => {
 
 app.listen(HTTP_PORT, () => {
     console.log(`Service HTTP: ${HTTP_PORT} listening...`);
+    p2pservice.listen();
 });
